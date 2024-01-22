c49970d37b683899288e824e74e25037b2d913cb
Juho Lee <dlwngh1367@gmail.com>
2024-01-22 17:07:19 -0500

    Google, Github Oauth added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import GoogleProvider from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
 import { compare } from 'bcrypt';
 import prismadb from '@/lib/prismadb';
+import {PrismaAdapter} from '@next-auth/prisma-adapter';
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -55,6 +56,7 @@ export const authOptions: AuthOptions = {
     signIn: '/auth'
   },
   debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: 'jwt' },
 
   secret: process.env.NEXTAUTH_SECRET
